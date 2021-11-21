e31a212c3dac8468a1206df10346cfb457d869f6
Juseung Lee <41817239+juuseung@users.noreply.github.com>
2021-11-21 00:16:49 -0600

    Update set_attr.c

--- assign3/set_attr.c
+++ assign3/set_attr.c
@@ -6,23 +6,23 @@
 #include "expr.h"
 #include "tables.h"
 
-void setString(Schema *schema, int attrNum, Value *value, char *dataptr) {
-	int length = schema->typeLength[attrNum]; // set variable as the typeLength of attribute
-	strncpy(dataptr, value->v.stringV, length); // copy string from variable with amount of the typeLength to data
-	dataptr += schema->typeLength[attrNum]; // reset the current location of the data array by adding the size of typeLength of the attribute
+void setFloat(Schema *schema, int attrNum, Value *value, char *ptr) {
+	*(float *) ptr = value->v.floatV;
+	ptr += sizeof(float); // reset the current location of the data array by adding the size of typeLength of the attribute
 }
 
-void setInt(Schema *schema, int attrNum, Value *value, char *dataptr) {
-	*(int *) dataptr = value->v.intV; // set data with variable
-	dataptr += sizeof(int); // reset the current location of the data array by adding the size of typeLength of the attribute
+void setBool(Schema *schema, int attrNum, Value *value, char *ptr) {
+	*(bool *) ptr = value->v.boolV;
+	ptr += sizeof(bool); // reset the current location of the data array by adding the size of typeLength of the attribute
 }
 
-void setFloat(Schema *schema, int attrNum, Value *value, char *dataptr) {
-	*(float *) dataptr = value->v.floatV; // set data with variable
-	dataptr += sizeof(float); // reset the current location of the data array by adding the size of typeLength of the attribute
+void setString(Schema *schema, int attrNum, Value *value, char *ptr) {
+	int size = schema->typeLength[attrNum];
+	strncpy(ptr, value->v.stringV, size); // copy string from variable with amount of the typeLength to data
+	ptr += schema->typeLength[attrNum]; // reset the current location of the data array by adding the size of typeLength of the attribute
 }
 
-void setBool(Schema *schema, int attrNum, Value *value, char *dataptr) {
-	*(bool *) dataptr = value->v.boolV; // set data with variable
-	dataptr += sizeof(bool); // reset the current location of the data array by adding the size of typeLength of the attribute
+void setInt(Schema *schema, int attrNum, Value *value, char *ptr) {
+	*(int *) ptr = value->v.intV;
+	ptr += sizeof(int); // reset the current location of the data array by adding the size of typeLength of the attribute
 }
